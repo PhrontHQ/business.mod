a5aa2fdaccb7276b32fad95878f66f772b48335a
Benoit Marchant <marchant@mac.com>
2022-04-15 19:25:15 -0700

    use create or replace as a workaround for now

--- data/main.datareel/raw-model/install-postGIS.sql
+++ data/main.datareel/raw-model/install-postGIS.sql
@@ -12,13 +12,7 @@ alter schema tiger owner to rds_superuser;
 alter schema tiger_data owner to rds_superuser;
 alter schema topology owner to rds_superuser;
 
-do $$
-begin
-    CREATE FUNCTION exec(text) returns text language plpgsql volatile AS $f$ BEGIN EXECUTE $1; RETURN $1; END; $f$;
-  exception
-    when duplicate_function then
-    null;
-end; $$
+CREATE OR REPLACE FUNCTION exec(text) returns text language plpgsql volatile AS $f$ BEGIN EXECUTE $1; RETURN $1; END; $f$;
 
 SELECT exec('ALTER TABLE ' || quote_ident(s.nspname) || '.' || quote_ident(s.relname) || ' OWNER TO rds_superuser;')
 FROM (
