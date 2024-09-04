e2b409b805c996c919abc5a73cace2c03c77da8f
Benoit Marchant <marchant@mac.com>
2024-09-04 00:12:04 -0700

    - add DataObjectType (hierarchical) as superclass of object's types - Update EmploymentType to subclass it - create OrganizationType - update Organization's type from string to OrganizationType

---
+++ data/main.mod/model/organization-type.js
@@ -0,0 +1,14 @@
+var DataObjectType = require("/data-object-type").DataObjectType;
+/**
+ * @class OrganizationType
+ * @extends DataObjectType
+ */
+
+
+exports.OrganizationType = DataObjectType.specialize(/** @lends OrganizationType.prototype */ {
+
+    organizations: {
+        value: undefined
+    }
+
+});
