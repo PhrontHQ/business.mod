09ef1e2e4a1c1c6aa14b7e79cdaeda851c96af6b
Benoit Marchant <marchant@mac.com>
2025-04-30 04:01:42 -0700

    add simple/partial/temporary deserializeSelf for a specific use case

--- data/main.mod/model/organization.js
+++ data/main.mod/model/organization.js
@@ -57,6 +57,28 @@ exports.Organization = Party.specialize(/** @lends Organization.prototype */ {
     },
     applications: {
         value: undefined
+    },
+
+    /*
+        Partial because temporary
+    */
+    deserializeSelf: {
+        value: function(deserializer) {
+            this.super(deserializer);
+
+            var value;
+            value = deserializer.getProperty("name");
+            if (value !== void 0) {
+                this.name = value;
+            }
+
+            value = deserializer.getProperty("identifier");
+            if (value !== void 0) {
+                this.identifier = value;
+            }
+
+        }
     }
+
 
 });
