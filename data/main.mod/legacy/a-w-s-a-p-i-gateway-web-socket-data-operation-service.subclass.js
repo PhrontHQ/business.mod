2b88d0dd9b9be6fe442d974231fa0c059a28ee85
Benoit Marchant <marchant@mac.com>
2025-09-19 16:32:42 -0700

    remove legacy

--- data/main.mod/legacy/a-w-s-a-p-i-gateway-web-socket-data-operation-service.subclass.js
+++
@@ -1,9 +0,0 @@
-var WebSocketDataOperationService = require("mod/data/service/web-socket-data-operation-service").WebSocketDataOperationService;
-
-/**
-* TODO: Document
-*
-* @class
-* @extends RawDataService
-*/
-exports.AWSAPIGatewayWebSocketDataOperationService = AWSAPIGatewayWebSocketDataOperationService = WebSocketDataOperationService.specialize(/** @lends AWSAPIGatewayWebSocketDataOperationService.prototype */ {});
